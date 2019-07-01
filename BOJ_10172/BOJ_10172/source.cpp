49f6eb3762c819b0c8356c44e19614839316033a
hhhan0315 <hhan0315@naver.com>
2019-07-01 18:15:29 +0900

    printf example

---
+++ BOJ_10172/BOJ_10172/source.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	printf("|\\_/|\n");
+	printf("|q p|   /}\n");
+	printf("( 0 )\"\"\"\\\n");
+	printf("|\"^\"`    |\n");
+	printf("||_/=\\\\__|\n");
+	return 0;
+	// \만 출력하고 싶을 때 \\ 사용
+	// "만 출력하고 싶을 때 \" 사용
+}
