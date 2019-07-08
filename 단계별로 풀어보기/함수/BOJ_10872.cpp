2fb8adbcd6ca112ce33eb7fb5f40759f2fcc10d6
hhhan0315 <hhan0315@naver.com>
2019-07-08 19:05:32 +0900

    팩토리얼

---
+++ 단계별로 풀어보기/함수/BOJ_10872.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int factorial(int n) {
+	if (n == 0) return 1;
+	else return n * factorial(n - 1);
+}
+
+int main() {
+	int num;
+	scanf("%d", &num);
+	printf("%d", factorial(num));
+	return 0;
+}
