7a00964834406d8a1561c3b27ef126e8c96d4334
hhhan0315 <hhan0315@naver.com>
2019-07-10 21:17:55 +0900

    주석 수정

--- 단계별로 풀어보기/수학 1/BOJ_1011.cpp
+++ 단계별로 풀어보기/수학 1/BOJ_1011.cpp
@@ -1,10 +1,10 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 int main() {
 	int n;
 	scanf("%d", &n);
 
 	for (int i = 0; i<n; i++) {
-		unsigned int start, end; //unsigned int : always plus int
+		unsigned int start, end; //unsigned int : 항상 양수인 정수
 		scanf("%d %d", &start, &end);
 		int a = 1;
 		while (1) {
