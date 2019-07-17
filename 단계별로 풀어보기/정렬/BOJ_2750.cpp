c72161ee9e75dda06abceb6e2e1e58dc1435578c
hhhan0315 <hhan0315@naver.com>
2019-07-17 17:19:21 +0900

    수 정렬하기(시간복잡도 O(n^2))

---
+++ 단계별로 풀어보기/정렬/BOJ_2750.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() {
+	int num;
+	scanf("%d", &num);
+	int arr[1000];
+	int result = 0;
+
+	for (int i = 0; i<num; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (int j = 0; j<num; j++) {
+		for (int k = 0; k<num - 1; k++)
+		{
+			if (arr[k]>arr[k + 1])
+			{
+				result = arr[k];
+				arr[k] = arr[k + 1];
+				arr[k + 1] = result;
+			}
+		}
+	}
+
+	for (int i = 0; i<num; i++) {
+		printf("%d\n", arr[i]);
+	}
+	return 0;
+}
+//버블정렬 활용
