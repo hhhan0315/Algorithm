00e068cb112bb6adcf734c0485fda98b08d519fa
hhhan0315 <31765530+hhhan0315@users.noreply.github.com>
2019-12-26 17:16:57 +0900

    가장 긴 증가하는 부분 수열

---
+++ 알고리즘 공부/DP/BOJ_11053.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int arr[1001];
+int dp[1001];
+
+int main() {
+	int N;
+	int max = 0;
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j]) {
+				if (dp[i] < dp[j] + 1) {
+					dp[i] = dp[j] + 1;
+				}
+			}
+		}
+		if (max < dp[i])
+			max = dp[i];
+	}
+	cout << max <<endl;
+	return 0;
+}
