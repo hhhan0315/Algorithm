9ce2e5361d41082a667bec5c4ce0d2bc98d50ba6
Han Seung Rae <31765530+hhhan0315@users.noreply.github.com>
2020-04-22 15:56:44 +0900

    level 2 study

---
+++ Programmers/LEVEL_2/H-Index.cpp
@@ -0,0 +1,19 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    // h회 이상이 h개인지 확인
+    for (int i = 0; i < citations.size(); i++)
+    {
+        if (citations[i] > answer) answer++;
+        else break;
+    }
+
+    return answer;
+}
---
+++ Programmers/LEVEL_2/가장 큰 수.cpp
@@ -0,0 +1,30 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool compare(string a, string b)
+{
+    return a + b > b + a; // 핵심
+}
+
+// string으로 합치고 비교
+string solution(vector<int> numbers) {
+    string answer = "";
+    vector<string> temp;
+    for (auto num : numbers)
+    {
+        temp.push_back(to_string(num));
+    }
+    sort(temp.begin(), temp.end(), compare);
+
+    if (temp.at(0) == "0") return "0"; // [0, 0, 0] 일 때 000 나오는 것 방지 
+
+    for (auto num : temp)
+    {
+        answer += num;
+    }
+
+    return answer;
+}
